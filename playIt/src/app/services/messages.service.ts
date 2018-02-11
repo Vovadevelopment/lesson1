eaaf8158cd82beb41d61aa1793eb6e7e35089c39
Vova <vova@deepinspire.com>
2018-02-11 17:24:19 +0200

    messages service

--- playIt/src/app/services/messages.service.ts
+++ playIt/src/app/services/messages.service.ts
@@ -3,11 +3,13 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class MessagesService {
   private messages: any[] = [];
+  id: number = 0;
   constructor() {}
   getMessages() {
     return [...this.messages];
   }
   addMessage(title, message) {
-    this.messages.push({title: title, message: message});
+    this.messages.push({title: title, message: message, time: new Date(), id: this.id});
+    this.id++;
   }
 }
