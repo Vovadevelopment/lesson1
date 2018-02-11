98ef6e88273d1569999fb2fe3d819b4763ffd152
Vova <vova@deepinspire.com>
2018-02-11 17:47:40 +0200

    s

--- playIt/src/app/services/messages.service.ts
+++ playIt/src/app/services/messages.service.ts
@@ -3,17 +3,17 @@ import {Message} from './message.model';
 
 @Injectable()
 export class MessagesService {
-  private messages: any[] = [];
+  private messages: Message[] = [];
   id: number = 0;
   constructor() {}
-  getMessages() {
+  getMessages(): Message[] {
     return [...this.messages];
   }
-  addMessage(title, message) {
+  addMessage(title, message: Message): void {
     this.messages.push(new Message(title, message, new Date(), this.id));
     this.id++;
   }
-  editMessage(id, editedMessage) {
+  editMessage(id, editedMessage): void {
     for(let i = 0; i < this.messages.length; i++) {
       if (this.messages[i].id === id) {
         Object.assign(this.messages[i], editedMessage);
